62dceecd6c574dd40f33f6607888c15f824e282a
YC.W <contact@wangyuchen.cn>
2024-01-14 20:50:13 +0800

    chore: add svg support

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { fileURLToPath } from 'node:url'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { fileURLToPath } from "node:url"
+import svgr from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url))
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
       },
-    ]
-  }
+    ],
+  },
 })
