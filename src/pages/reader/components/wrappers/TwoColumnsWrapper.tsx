5d900b50d4642eacab284d72d55abc17179e0de6
YC.W <contact@wangyuchen.cn>
2024-01-20 01:35:07 +0800

    chore: add drag button (placeholder)

--- src/pages/reader/components/wrappers/TwoColumnsWrapper.tsx
+++ src/pages/reader/components/wrappers/TwoColumnsWrapper.tsx
@@ -1,12 +1,30 @@
+import Icon from "@ant-design/icons"
+import { Button, Popover } from "antd"
+import dragSvg from "@/pages/reader/images/drag.svg?react"
+
 type Props = {
   left?: React.ReactNode
   right?: React.ReactNode
 }
 export function TwoColumnsWrapper({ left, right }: Props) {
   return (
-    <div className="grid grid-cols-2 gap-4 w-full">
-      <div className="">{left}</div>
-      <div className="">{right}</div>
-    </div>
+    <Popover
+      trigger="hover"
+      content={
+        <Button type="text" icon={<Icon component={dragSvg} />}></Button>
+      }
+      overlayInnerStyle={{
+        backgroundColor: "transparent",
+        padding: 0,
+        boxShadow: "none",
+      }}
+      placement="leftTop"
+      arrow={false}
+    >
+      <div className="grid grid-cols-2 gap-4 w-full ml-10">
+        <div className="">{left}</div>
+        <div className="">{right}</div>
+      </div>
+    </Popover>
   )
 }
