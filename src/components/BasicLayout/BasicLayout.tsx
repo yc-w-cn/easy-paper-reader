e0571a46ee37670f42d32ab90ff82802e00793d3
YC.W <contact@wangyuchen.cn>
2024-01-21 03:09:50 +0800

    chore: add custom mode

--- src/components/BasicLayout/BasicLayout.tsx
+++ src/components/BasicLayout/BasicLayout.tsx
@@ -2,18 +2,23 @@ import { PageWrapper, PageSidebar, PageContent, MenuBar } from "@/components"
 import { ReactNode } from "react"
 
 type Props = {
+  mode?: "center" | "custom"
   children?: ReactNode
 }
 
-export function BasicLayout({ children }: Props) {
+export function BasicLayout({ children, mode = "center" }: Props) {
   return (
     <PageWrapper>
       <PageSidebar />
       <PageContent>
         <MenuBar />
-        <div className="flex justify-center items-center h-full">
-          {children}
-        </div>
+        {mode === "center" ? (
+          <div className="flex flex-grow justify-center items-center">
+            {children}
+          </div>
+        ) : (
+          children
+        )}
       </PageContent>
     </PageWrapper>
   )
