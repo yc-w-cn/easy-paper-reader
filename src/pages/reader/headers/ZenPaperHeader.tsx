e929d17f149100c0f34740bbf487419dfcb08504
YC.W <contact@wangyuchen.cn>
2024-01-21 15:28:34 +0800

    fix: margin

--- src/pages/reader/headers/ZenPaperHeader.tsx
+++ src/pages/reader/headers/ZenPaperHeader.tsx
@@ -2,16 +2,23 @@ import { FloatButton } from "antd"
 import { LogoutOutlined } from "@ant-design/icons"
 import { useReaderDispatch, useReaderSelector } from "@/stores"
 import { selectPaper } from "@/features/reader/paper"
-import { changeMode } from "@/features/reader/layout"
+import { changeMode, selectLayoutColumnMode } from "@/features/reader/layout"
+import { cn } from "@/utils/tailwind"
 
 export function ZenPaperHeader() {
   const paper = useReaderSelector(selectPaper)
+  const columnMode = useReaderSelector(selectLayoutColumnMode)
   const dispatch = useReaderDispatch()
 
   if (!paper) return <></>
 
   return (
-    <header className="px-14 pt-4 relative flex gap-4 justify-between items-center">
+    <header
+      className={cn(
+        columnMode === "one" ? "px-20" : "px-14",
+        "pt-4 relative flex gap-4 justify-between items-center",
+      )}
+    >
       <div className="flex-1">
         <h1 className="text-lg leading-4 font-normal">{paper.title}</h1>
         <h2 className="text-sm text-gray-500 leading-4 font-normal">
