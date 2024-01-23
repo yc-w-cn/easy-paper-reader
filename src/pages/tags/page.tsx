a4cfb004848c1f6ac3cab1bb0fd492194275d57a
YC.W <contact@wangyuchen.cn>
2024-01-23 17:59:14 +0800

    feat: add refresh button

--- src/pages/tags/page.tsx
+++ src/pages/tags/page.tsx
@@ -1,16 +1,28 @@
-import { getTagsSnapshot } from "@/apis/local-data/tag"
+import { getTagsSnapshot, refreshTagsSnapshot } from "@/apis/local-data/tag"
 import { BasicLayout } from "@/components"
+import { ReloadOutlined } from "@ant-design/icons"
 import { useQuery } from "@tanstack/react-query"
-import { Flex, Tag } from "antd"
+import { App, Button, Flex, Tag } from "antd"
 
 export function TagsPage() {
+  const { message } = App.useApp()
   const response = useQuery({
     queryKey: ["tags-snapshot"],
     queryFn: () => getTagsSnapshot(),
   })
   return (
     <BasicLayout mode="center">
-      <Flex align="center" justify="center">
+      <Flex align="center" justify="center" gap={5}>
+        <Button
+          onClick={() => {
+            refreshTagsSnapshot()
+            response.refetch()
+            message.success("操作成功")
+          }}
+          type="text"
+          size="small"
+          icon={<ReloadOutlined />}
+        />
         <strong>全部标签：</strong>
         {response.data?.map((tag, index) => (
           <Tag key={`tag-${index}`}>{tag}</Tag>
