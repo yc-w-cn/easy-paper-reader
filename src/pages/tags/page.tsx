8d7605d5e2e186db930425c90e4a95252597d687
YC.W <contact@wangyuchen.cn>
2024-01-28 02:31:00 +0800

    chore: add empty placeholder

--- src/pages/tags/page.tsx
+++ src/pages/tags/page.tsx
@@ -1,21 +1,27 @@
 import { getTagsSnapshot, refreshTagsSnapshot } from "@/apis/local-data/tag"
 import { BasicLayout } from "@/components"
 import { ReloadOutlined } from "@ant-design/icons"
-import { useQuery } from "@tanstack/react-query"
-import { App, Button, Flex, Tag } from "antd"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import { App, Button, Empty, Flex, Tag } from "antd"
+
+const QUERY_KEY = "tags-snapshot"
 
 export function TagsPage() {
   const { message } = App.useApp()
+  const queryClinet = useQueryClient()
+
   const response = useQuery({
-    queryKey: ["tags-snapshot"],
+    queryKey: [QUERY_KEY],
     queryFn: () => getTagsSnapshot(),
   })
+
   return (
     <BasicLayout mode="center">
       <Flex align="center" justify="center" gap={5}>
         <Button
           onClick={() => {
             refreshTagsSnapshot()
+            queryClinet.invalidateQueries({ queryKey: [QUERY_KEY] })
             response.refetch()
             message.success("操作成功")
           }}
@@ -23,10 +29,16 @@ export function TagsPage() {
           size="small"
           icon={<ReloadOutlined />}
         />
-        <strong>全部标签：</strong>
-        {response.data?.map((tag, index) => (
-          <Tag key={`tag-${index}`}>{tag}</Tag>
-        ))}
+        {!(response.data?.length || 0) ? (
+          <>
+            <strong>全部标签：</strong>
+            {response.data?.map((tag, index) => (
+              <Tag key={`tag-${index}`}>{tag}</Tag>
+            ))}
+          </>
+        ) : (
+          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+        )}
       </Flex>
     </BasicLayout>
   )
