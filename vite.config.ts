9f3ebee08801253a4540080bcc1819e356108177
YC.W <contact@wangyuchen.cn>
2024-02-02 00:27:01 +0800

    feat: add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react-swc"
 import { fileURLToPath } from "node:url"
 import svgr from "vite-plugin-svgr"
 import version from "vite-plugin-package-version"
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === "gh-pages" ? "/easy-paper-reader" : "/",
-  plugins: [react(), svgr(), version()],
+  plugins: [react(), svgr(), version(), visualizer()],
   resolve: {
     alias: [
       {
