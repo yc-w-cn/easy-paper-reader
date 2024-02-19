56941c650f78e32db564215a168ea410673a0469
YC.W <contact@wangyuchen.cn>
2024-02-19 08:48:41 +0800

    chore: default sort by ctime desc

--- src/apis/local-data/history/snapshot.ts
+++ src/apis/local-data/history/snapshot.ts
@@ -4,8 +4,12 @@ import { CacheHistoryItem } from "./cache-history-item.type"
 import { HistoryAction } from "./history-action.enum"
 
 export async function getHistorySnapshot() {
-  const records = await localforage.getItem<CacheHistoryItem[]>("history-snapshot")
-  return records || []
+  const records =
+    await localforage.getItem<CacheHistoryItem[]>("history-snapshot")
+  if (records) {
+    return records.sort((item1, item2) => item2.ctime - item1.ctime)
+  }
+  return []
 }
 
 export async function refreshHistorySnapshot() {
